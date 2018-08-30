faec19d85c186ac970247497f6d74d4ab5a953f0
MaxONu4 <maxym.shylo@gmail.com>
2018-08-30 15:07:13 +0300

    admin/bids + sale/home controllers

--- app/Http/Controllers/Sale/HomeController.php
+++ app/Http/Controllers/Sale/HomeController.php
@@ -2,18 +2,12 @@
 
 namespace App\Http\Controllers\Sale;
 
+use App\Bid;
 use App\Source;
 use App\Status;
 use App\Timing;
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
-use Google_Client;
-use Google_Service_Sheets;
-use Revolution\Google\Sheets\Sheets;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
@@ -41,59 +35,7 @@ class HomeController extends Controller
             return redirect('/admin/bids');
         }
 
-
-        $client = new Google_Client();
-        $client->setApplicationName(config('google.APPLICATION_NAME'));
-        $client->setClientId(config('google.CLIENT_ID'));
-        $client->setScopes([config('google.scopes')]);
-//        $client->setAuthConfig(config('google.KEY_FILE'));
-
-        // credentials - required
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=../service.json'); // service account json file
-        $client->useApplicationDefaultCredentials();
-
-        if ($client->isAccessTokenExpired()) {
-            $client->refreshTokenWithAssertion();
-        }
-
-        $service_token = $client->getAccessToken();
-
-        $service = new Google_Service_Sheets($client);
-
-        $sheets = new Sheets();
-
-        $sheets->setService($service);
-
-        $id = '1o3kjb_wX3GeUXP5HxdDUwunfFzMUf81fW83a16Na0oI';
-        $sheet = 'sent offers';
-        $data = array(array('abc', 'def', 'ghi'), array('jkl', 'mno', 'prs'));
-
-
-
-//        $test = $sheets->spreadsheet($id)->sheetById($id);
-//        $test->addEditor('zelloooo1997@gmail.com');
-//        $edit = $test->getEditors();
-//        dd($edit);
-
-        $rows = $sheets->spreadsheet($id)->sheet($sheet)->all(); //spreadsheetID is from URL of your google spreadsheet, sheet name is sheet inside it
-
-//        $sheets->spreadsheet($id)->sheet($sheet)->append($data);
-
-        /* Update sheet in Spreadsheet */
-//        $sheets->sheet($sheet)->range('B1')->update([[90],[01.2]]);
-        /* finish */
-
-        $rows = $sheets->sheet($sheet)->all();
-
-        $array = [];
-
-        foreach ($rows as $row)
-        {
-            if ($row[1] == auth()->user()->name) {
-                $array[] = $row;
-//                dd($row);
-            }
-        }
+        $array = array_reverse(Bid::where('user_id', auth()->user()->id)->get()->toArray());
 
         $sourses = Source::all();
         $statuses = Status::all();
@@ -104,40 +46,9 @@ class HomeController extends Controller
 
     public function show()
     {
-        $client = new Google_Client();
-        $client->setApplicationName(config('google.APPLICATION_NAME'));
-        $client->setClientId(config('google.CLIENT_ID'));
-        $client->setScopes([config('google.scopes')]);
+        $array = Bid::where('user_id', auth()->user()->id)->get();
 
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=../service.json'); // service account json file
-        $client->useApplicationDefaultCredentials();
-
-        if ($client->isAccessTokenExpired()) {
-            $client->refreshTokenWithAssertion();
-        }
-
-        $service = new Google_Service_Sheets($client);
-
-        $sheets = new Sheets();
-        $sheets->setService($service);
-
-        $id = '1o3kjb_wX3GeUXP5HxdDUwunfFzMUf81fW83a16Na0oI';
-        $sheet = 'sent offers';
-
-        $rows = $sheets->spreadsheet($id)->sheet($sheet)->all();
-
-        $array = [];
-
-        foreach ($rows as $row)
-        {
-            if ($row[1] == auth()->user()->name) {
-                $array[] = $row;
-//                dd($row);
-            }
-        }
-
-//        return array_reverse($rows);
-        return array_reverse($array);
+        return array_reverse($array->toArray());
     }
 
     public function getAgent()
@@ -160,101 +71,35 @@ class HomeController extends Controller
 
     public function store(Request $request)
     {
-        $client = new Google_Client();
-        $client->setApplicationName(config('google.APPLICATION_NAME'));
-        $client->setClientId(config('google.CLIENT_ID'));
-        $client->setScopes([config('google.scopes')]);
-//        $client->setAuthConfig(config('google.KEY_FILE'));
 
-        // credentials - required
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=../service.json'); // service account json file
-        $client->useApplicationDefaultCredentials();
+        $arr = Bid::create([
+            'user_id' => auth()->user()->id,
+            'date' => date("d.m.Y"),
+            'agent' => auth()->user()->name,
+            'source' => request('source'),
+            'link' => request('link'),
+            'niche' => request('niche'),
+            'current' => request('site'),
+            'description' => request('desc'),
+            'timing' => request('timing'),
+            'budget' => request('budget'),
+            'response' => request('resp'),
+            'status' => request('status'),
+            'comment' => request('comment')
+        ]);
 
-        if ($client->isAccessTokenExpired()) {
-            $client->refreshTokenWithAssertion();
-        }
-
-        $service = new \Google_Service_Sheets($client);
-
-        $sheets = new Sheets();
-        $sheets->setService($service);
-
-        $id = '1o3kjb_wX3GeUXP5HxdDUwunfFzMUf81fW83a16Na0oI';
-        $sheet = 'sent offers';
-
-        $arr = [
-            date("d.m.Y"),
-            auth()->user()->name,
-            request('source'),
-            request('link'),
-            request('niche'),
-            request('site'),
-            request('desc'),
-            request('timing'),
-            request('budget'),
-            request('resp'),
-            request('status'),
-            request('comment')
-        ];
-
-        $data = array($arr);
-
-        $sheets->spreadsheet($id)->sheet($sheet)->range('')->append($data);
-
-        return $data;
+        return $arr;
     }
 
     public function update(Request $request)
     {
-        $client = new Google_Client();
-        $client->setApplicationName(config('google.APPLICATION_NAME'));
-        $client->setClientId(config('google.CLIENT_ID'));
-        $client->setScopes([config('google.scopes')]);
-//        $client->setAuthConfig(config('google.KEY_FILE'));
 
-        // credentials - required
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=../service.json'); // service account json file
-        $client->useApplicationDefaultCredentials();
+        $bid = Bid::where('user_id', auth()->user()->id)->where('id', request('data')['id'])->first();
 
-        if ($client->isAccessTokenExpired()) {
-            $client->refreshTokenWithAssertion();
-        }
+        $bid->update([
+            'status' => request('data')['10']
+        ]);
 
-        $service = new \Google_Service_Sheets($client);
-
-        $sheets = new Sheets();
-        $sheets->setService($service);
-
-        $id = '1o3kjb_wX3GeUXP5HxdDUwunfFzMUf81fW83a16Na0oI';
-        $sheet = 'sent offers';
-
-        $rows = $sheets->spreadsheet($id)->sheet($sheet)->all();
-
-//        $rows = array_reverse($rows);
-
-        $array = [];
-
-        foreach ($rows as $row)
-        {
-            if ($row[1] == auth()->user()->name) {
-                $array[] = $row;
-//                dd($row);
-            }
-        }
-
-        $array = array_reverse($array);
-
-        $c = 0;
-        for ($i=1; $i<count($rows); $i++) {
-
-            if ($rows[$i] === $array[request('index')])
-            {
-                $c = $i + 1;
-            }
-
-        }
-
-        $sheets->sheet($sheet)->range('K'.$c)->update([[request('data')['10']]]);
 
     }
 }
