357b09d79eb2826b8ad52270fed883fc6c4d78ff
MaxONu4 <maxym.shylo@gmail.com>
2018-09-08 13:12:50 +0300

    user bids

--- app/Http/Controllers/Admin/BidsController.php
+++ app/Http/Controllers/Admin/BidsController.php
@@ -17,36 +17,13 @@ class BidsController extends Controller
 {
     public function index()
     {
-        $client = new Google_Client();
-        $client->setApplicationName(config('google.APPLICATION_NAME'));
-        $client->setClientId(config('google.CLIENT_ID'));
-        $client->setScopes([config('google.scopes')]);
-
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=../service.json'); // service account json file
-        $client->useApplicationDefaultCredentials();
-
-        if ($client->isAccessTokenExpired()) {
-            $client->refreshTokenWithAssertion();
-        }
-
-        $service = new Google_Service_Sheets($client);
-
-        $sheets = new Sheets();
-        $sheets->setService($service);
-
-        $id = '1o3kjb_wX3GeUXP5HxdDUwunfFzMUf81fW83a16Na0oI';
-        $sheet = 'sent offers';
-
-        $rows = $sheets->spreadsheet($id)->sheet($sheet)->all();
-
-        $rows = array_reverse($rows);
+        $rows = array_reverse(Bid::all()->toArray());
 
         return view('admin.index', compact('rows'));
     }
 
     public function show()
     {
-
         $rows = Bid::all()->toArray();
 
         return array_reverse($rows);
