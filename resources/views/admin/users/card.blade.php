22bd1a512ccc6196c1b23e805ca3a283f9e621a8
djkey245 <djkey245@gmail.com>
2018-09-10 16:02:54 +0300

    Add card user in Admin - Worksection integration

--- resources/views/admin/users/card.blade.php
+++ resources/views/admin/users/card.blade.php
@@ -3,7 +3,10 @@
 @section('content')
     <div class="home-page page">
         <div id="app-vue" class="wrapper p-3">
-            {{dd($users_projects)}}
+
+            {{--{{dd($users_projects)}}--}}
+            <user-card :projects="{{json_encode($users_projects)}}"></user-card>
+
 
         </div>
     </div>
