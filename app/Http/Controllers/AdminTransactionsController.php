0e5087ce6f0cbb7a26f31811da93be0aec8eb2e3
MaxONu4 <maxym.shylo@gmail.com>
2018-09-21 14:49:51 +0300

    transactions model, migrations...

---
+++ app/Http/Controllers/AdminTransactionsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AdminTransaction;
+use Illuminate\Http\Request;
+
+class AdminTransactionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\AdminTransaction  $adminTransactions
+     * @return \Illuminate\Http\Response
+     */
+    public function show(AdminTransaction $adminTransactions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\AdminTransaction  $adminTransactions
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(AdminTransaction $adminTransactions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\AdminTransaction  $adminTransactions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, AdminTransaction $adminTransactions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\AdminTransaction  $adminTransactions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(AdminTransaction $adminTransactions)
+    {
+        //
+    }
+}
