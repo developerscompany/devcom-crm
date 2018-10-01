8255750be9301f1eb1690f4501628a20eb96461d
djkey245 <djkey245@gmail.com>
2018-10-01 18:09:19 +0300

    Update Hosting, add react Edit for Hosting account and End work on react Conditions for hosting account

--- routes/admin/web.php
+++ routes/admin/web.php
@@ -29,6 +29,7 @@ Route::prefix('admin')->namespace('Admin')->middleware('admin')->group(function 
             Route::get('/', 'HostingController@index');
             Route::get('/account/{hosting}', 'HostingController@show');
             Route::post('/account/{hosting}/add-condition', 'HostingController@conditionAdd');
+            Route::post('/account/{hosting}/remove-condition/{condition}', 'HostingController@conditionRemove');
             Route::post('/account/{hosting}/comment', 'HostingController@getComment');
             Route::get('/account/{hosting}/edit', 'HostingController@edit');
             Route::get('/account/{hosting}/sale', 'HostingController@getSale');
