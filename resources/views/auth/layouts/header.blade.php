d2b96857bfd2f5dc3ce0eba95795580865933849
MaxONu4 <maxym.shylo@gmail.com>
2018-10-02 17:41:00 +0300

    styling

---
+++ resources/views/auth/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<header class="container my-0">
+
+    <!-- Main navbar -->
+    <div class="navbar navbar-inverse row">
+        <div class="navbar-header col-md-2">
+            <a class="navbar-brand" href="/"><img class="img-fluid" style="max-width: 170px;" src="/img/logo.png"></a>
+
+            <ul class="nav navbar-nav d-block d-sm-none">
+                <li><a data-toggle="collapse" data-target="#navbar-mobile"><i class="icon-tree5"></i></a></li>
+                <li><a class="sidebar-mobile-main-toggle"><i class="icon-paragraph-justify3"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="navbar-collapse col-md-3" id="navbar-mobile">
+            <ul class="nav navbar-nav">
+                <li><a class="sidebar-control sidebar-main-toggle hidden-xs"><i class="icon-paragraph-justify3"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- /main navbar -->
+
+</header>
---
+++ resources/views/auth/layouts/menu.blade.php
@@ -0,0 +1,45 @@
+<div class="sidebar sidebar-main">
+    <div class="user-pan my-4">
+        <span class="pull-left mr-3">
+            @if ($avatar = auth()->user()->avatar)
+                @if (is_role('admin'))
+                    <a href="/admin"><img src="{{ asset('storage/' . $avatar) }}" class="img-rounded"></a>
+                @elseif (is_role('farmer'))
+                    <a href="/farm/edit/important"><img src="{{ asset('storage/' . $avatar) }}" class="img-rounded"></a>
+                @else
+                    <a href="/user"><img src="{{ asset('storage/' . $avatar) }}" class="img-rounded"></a>
+                @endif
+            @else
+                @if (is_role('admin'))
+                    <a href="/admin"><img class="img-fluid" src="/img/user.png"></a>
+                @elseif (is_role('farmer'))
+                    <a href="/farm/edit/important"><img class="img-fluid" src="/img/user.png"></a>
+                @else
+                    <a href="/user"><img class="img-fluid" src="/img/user.png"></a>
+                @endif
+            @endif
+        </span>
+        <div class="text-capitalize">
+            {{ auth()->user()->name }}
+        </div>
+        <div>
+            {{ auth()->user()->role }}
+        </div>
+    </div>
+    <div class="sidebar-content">
+        <ul class="nav navigation">
+            <li class="{{ is_active('user/cab') }}">
+                <a href="{{ url('user/cab') }}">
+                    <i class="icon-home4"></i>
+                    <span>Особистий кабінет</span>
+                </a>
+            </li>
+            <li class="{{ is_active('user') }}">
+                <a href="{{ url('user') }}">
+                    <i class="icon-home4"></i>
+                    <span>Біди</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
