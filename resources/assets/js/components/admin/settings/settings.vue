527b149ebf8b131373b62255a89d2c96766fe194
djkey245 <djkey245@gmail.com>
2018-10-05 14:04:46 +0300

    Add style for modul Settings

--- resources/assets/js/components/admin/settings/settings.vue
+++ resources/assets/js/components/admin/settings/settings.vue
@@ -1,16 +1,16 @@
 <template>
 
-    <div class="users">
+    <div class="settings-block">
 
         <div class="container-fluid">
             <h2>Хостинг.Послуги</h2>
-            <div class="row">
+            <div class="row table-head-field">
                 <div class="col-md-3">Назва(англ)</div>
                 <div class="col-md-3">Назва(укр)</div>
                 <div class="col-md-3">Класс(class)</div>
                 <div class="col-md-3">Події</div>
             </div>
-            <div class="row">
+            <div class="row ">
                 <div class="col-md-3">
                     <input type="text" class="form-control" v-model="cond.name">
                 </div>
@@ -24,11 +24,11 @@
 
                 <div class="col-md-3">
 
-                    <button class="btn" @click="saveConditions">Зберегти</button>
+                    <button class="btn btn-edit" @click="saveConditions">Зберегти</button>
                 </div>
 
             </div>
-            <div class="row" v-for="condition in conditions">
+            <div class="row table-content-field" v-for="condition in conditions">
                 <div class="col-md-3">{{condition.name}}</div>
                 <div class="col-md-3">{{condition.name_ua}}</div>
                 <div class="col-md-3">{{condition.class}}</div>
@@ -39,7 +39,7 @@
         </div>
         <div class="container-fluid">
             <h2>Валюта</h2>
-            <div class="row">
+            <div class="row table-head-field">
                 <div class="col-md-3">Назва(англ)</div>
                 <div class="col-md-3">Назва(укр)</div>
                 <div class="col-md-1">Символ</div>
@@ -63,11 +63,11 @@
 
                 <div class="col-md-3">
 
-                    <button class="btn" @click="saveCurrency">Зберегти</button>
+                    <button class="btn btn-edit" @click="saveCurrency">Зберегти</button>
                 </div>
 
             </div>
-            <div class="row" v-for="currency in currencies">
+            <div class="row table-content-field" v-for="currency in currencies">
                 <div class="col-md-3">{{currency.name}}</div>
                 <div class="col-md-3">{{currency.name_ua}}</div>
                 <div class="col-md-1">{{currency.symbol}}</div>
